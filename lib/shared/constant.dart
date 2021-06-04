3b4c76f40724b5ccd1ec9f7038d849a77e4569a1
lilran14 <radans.works@gmail.com>
2021-06-04 19:40:56 +0700

    Add Shared Variable

---
+++ lib/shared/constant.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Color
+Color kPrimary = Color(0xFF212121);
+Color kSecondary = Color(0xFF181818);
+
+// Color Extra
+Color kPrimaryActive = Color(0xFF363636);
+Color kPrimaryDisable = Color(0xFF858585);
+
+// Semantic Color
+Color kSemanticRed = Colors.red;
+Color kSematicGreen = Color(0xFF24FF00);
+
+// Text Color
+Color kTextBlack = Color(0xFF1a1a1a);
+Color kTextGrey = Color(0xFF858585);
